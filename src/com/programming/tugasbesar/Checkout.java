30a81f54383ff66b24c43fe0e8faa135f600545b
doantaa <doanta7aloycius@gmail.com>
2023-01-18 12:19:14 +0700

    Update Checkout UI Layout

--- src/com/programming/tugasbesar/Checkout.java
+++ src/com/programming/tugasbesar/Checkout.java
@@ -6,7 +6,7 @@ public class Checkout extends JFrame{
     private JRadioButton QRISRadioButton;
     private JRadioButton debitCardCreditCardRadioButton;
     private JRadioButton mobileBankingRadioButton;
-    private JButton lanjutKePembayaranButton;
+    private JButton payButton;
     private JPanel FilmDetails;
     private JPanel tiketDetailPanel;
     private JLabel filmTitleLabel;
@@ -24,6 +24,9 @@ public class Checkout extends JFrame{
     private JLabel paymentMethodLabel;
     private JLabel checkoutTitle;
     private JPanel checkoutPanel;
+    private JPanel paymentPanel;
+    private JPanel priceLabelPanel;
+    private JPanel pricePanel;
 
     public  Checkout(){
         //menentukan judul aplikasi
@@ -33,7 +36,7 @@ public class Checkout extends JFrame{
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //menentukan resolusi window/frame
-        this.setSize( 530,400);
+        this.setSize( 530,550);
 
         //menentukan panel mana yang akan dijadikan konten
         this.setContentPane(checkoutPanel);
